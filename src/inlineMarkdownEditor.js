bfb6d2f0c910fc868b0f5ffe7fa80009ba997387
jywarren <jeff@unterbahn.com>
2017-02-23 21:20:12 -0500

    initial commit

---
+++ src/inlineMarkdownEditor.js
@@ -0,0 +1,14 @@
+function inlineMarkdownEditor(o) {
+  var el = $(o.selector);
+  // split by double-newline:
+  var sections = el.html().split('\n\n');
+  el.html('');
+  processSections = require('./processSections.js');
+  processSections(sections, {
+    replaceUrl: o.replaceUrl,
+    selector: o.selector,
+    preProcessor: o.preProcessor,
+    postProcessor: o.postProcessor
+  });
+  el.show();
+}
---
+++ src/processSections.js
@@ -0,0 +1,11 @@
+function processSections(sections, o) {
+  sections.forEach(function(markdown) {
+    processSection = require('./processSection.js');
+    processSection(markdown, {
+      replaceUrl: o.replaceUrl,
+      selector: o.selector, 
+      postProcessor: o.postProcessor,
+      preProcessor: o.preProcessor
+    });
+  });
+}
