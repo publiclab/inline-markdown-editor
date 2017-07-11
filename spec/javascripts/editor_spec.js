5fd497476127e493cf38e8409e32a57643890e62
jywarren <jeff@unterbahn.com>
2017-07-11 11:54:18 -0400

    fix for 3x newline issue, solving test

--- spec/javascripts/editor_spec.js
+++ spec/javascripts/editor_spec.js
@@ -32,23 +32,23 @@ describe("Editor", function() {
   });
 
   it("generates the right number of sections", function() {
-    expect(editor.sections.length).toBe(7);
-    expect($('.inline-section').length).toBe(7);
+    expect(editor.sections.length).toBe(8);
+    expect($('.inline-section').length).toBe(8);
     expect(editor.sections[0]).toBe('');
-    expect(editor.editableSections.length).toBe(4);
-    expect($('.inline-edit-form').length).toBe(4);
-    expect($('.inline-edit-form textarea').length).toBe(4);
+    expect(editor.editableSections.length).toBe(5);
+    expect($('.inline-edit-form').length).toBe(5);
+    expect($('.inline-edit-form textarea').length).toBe(5);
   });
 
   it("correctly splits up sections", function() {
-    expect(editor.sections.length).toBe(7);
     expect(editor.sections[0]).toBe("");
     expect(editor.sections[1]).toBe("### Hello, World");
     expect(editor.sections[2]).toBe("This is a paragraph.");
     expect(editor.sections[3]).toBe("* this\n* is\n* a\n* list");
     expect(editor.sections[4]).toBe("****");
     expect(editor.sections[5]).toBe("### Hello again, World");
-    expect(editor.sections[6]).toBe("");
+    expect(editor.sections[6]).toBe("### Hello finally");
+    expect(editor.sections[7]).toBe("");
   });
 
 });
