c83a9ede6cb5e4d1278715f26a56f78fc4cce113
jywarren <jeff@unterbahn.com>
2019-02-21 11:47:01 -0800

    new test to fail on false

--- spec/javascripts/error_handling_spec.js
+++ spec/javascripts/error_handling_spec.js
@@ -1,5 +1,5 @@
 describe('error handling by onFail', function () {
-  
+
   var editor, html = "A simple text line";
 
   beforeEach(function() {
@@ -9,11 +9,14 @@ describe('error handling by onFail', function () {
       replaceUrl: '/wiki/replace/',
       selector: '.markdown'
     });
+    jasmine.Ajax.install();
+  });
+
+  afterEach(function() {
+    jasmine.Ajax.uninstall();
   });
 
   it("should call onFail", function(){
-    jasmine.Ajax.install();
-
     spyOn(editor.options, "onComplete");
     spyOn(editor.options, "onFail");
 
@@ -21,6 +24,25 @@ describe('error handling by onFail', function () {
       //here options is /wiki/replace/  
       var d = $.Deferred();
       d.reject("this is the response");     
+      return d.promise();
+    });
+  
+    $('.inline-edit-btn').click(); // generate editor by clicking the pencil icon
+    $('.inline-edit-form button.submit').click(); //click the save button in that form to send the post request
+
+    expect(editor.options.onComplete).not.toHaveBeenCalled();
+    expect(editor.options.onFail).toHaveBeenCalled();
+  });
+
+  // we are seeing response of "false" when plots2 fails to perform a replacement; adjusting here to read that as a failure
+  it("should correctly report 'false' string response as a failure, even with 200 response code", function(){
+    spyOn(editor.options, "onComplete");
+    spyOn(editor.options, "onFail");
+
+    spyOn($, "post").and.callFake(function(options) {
+      //here options is /wiki/replace/  
+      var d = $.Deferred();
+      d.done("false");     
       return d.promise();
     });
   
